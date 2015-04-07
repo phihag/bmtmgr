bdd66a38175ebe21924059771c21e6c10cf4910b
Philipp Hagemeister <phihag@phihag.de>
2015-04-07 02:20:47 +0200

    Very simple HTML table export

---
+++ controllers/tournament_entries.php
@@ -0,0 +1,37 @@
+<?php
+namespace bmtmgr;
+require_once dirname(__DIR__) . '/src/common.php';
+
+$u = user\check_current();
+$u->require_perm('admin');
+
+utils\require_get_params(['id']);
+$tournament = Tournament::by_id($_GET['id']);
+$season = $tournament->get_season();
+$disciplines = $tournament->get_disciplines();
+foreach ($disciplines as $d) {
+	$d->entries = $d->get_entries_rows_with_verbose_players();
+	$d->_is_new = 'modified';
+}
+
+$data = [
+	'user' => $u,
+	'breadcrumbs' => [
+		['name' => 'Ligen', 'path' => 'season/'],
+		['name' => $season->name, 'path' => 'season/' . $season->id . '/'],
+		['name' => $tournament->name, 'path' => 't/' . $tournament->id . '/'],
+		['name' => $tournament->name, 'path' => 't/entries' . $tournament->id . '/'],
+	],
+	'season' => $season,
+	'tournament' => $tournament,
+	'disciplines' => $disciplines,
+	'now_date' => \date('Y-m-d'),
+];
+
+// TODO actually offer it as a download (then with other styles)
+if (isset($_GET['standalone'])) {
+	echo get_rendered('tournament_entries_standalone', $data);
+} else {
+	render('tournament_entries', $data);
+}
+
