586a9780b270c81f71df8e51ccf7d66096fc7cde
Philipp Hagemeister <phihag@phihag.de>
2015-05-04 11:04:39 +0200

    club creation

---
+++ controllers/club_create.php
@@ -0,0 +1,24 @@
+<?php
+namespace bmtmgr;
+require_once dirname(__DIR__) . '/src/common.php';
+
+utils\csrf_protect();
+$u = user\check_current();
+$u->require_perm('admin');
+
+utils\require_post_params(['name']);
+$textid = \preg_replace('/[^a-z]+/', '', strtolower($_POST['name']));
+assert(\preg_match('/^[a-z]+$/', $textid));
+try {
+	$club = User::create($textid, $_POST['name'], null);
+	$club->save();
+} catch (utils\DuplicateEntryException $e) {
+	render_ajax_error(
+		'Der Verein "' . $_POST['name'] . '" existiert bereits'
+	);
+	exit();
+}
+
+render_ajax('club/' . $club->id . '/', [
+	'club' => $club
+]);
