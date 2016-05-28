337016646a2497c3662b2489d58ad7d52e921944
Philipp Hagemeister <phihag@phihag.de>
2016-05-29 00:02:47 +0200

    aentry: start implementing player selection

--- controllers/discipline_player_search.php
+++ controllers/discipline_player_search.php
@@ -2,10 +2,17 @@
 namespace bmtmgr;
 require_once dirname(__DIR__) . '/src/common.php';
 
-$u = user\check_current();
-$u->require_perm('admin');
-
 utils\require_get_params(['discipline_id', 'gender', 'term']);
+
+// TODO optimize these two lines to one
+$d = Discipline::by_id($_GET['discipline_id']);
+$t = $d->get_tournament();
+
+if (! $t->visible) {
+	$u = user\check_current();
+	$u->require_perm('admin');
+}
+
 $player_rows = Discipline::suggest_player_rows_with_clubs_by_id(
 	$_GET['discipline_id'], $_GET['term'], $_GET['gender']);
 
--- static/aentry.js
+++ static/aentry.js
@@ -1,3 +1,5 @@
+'use strict';
+
 var uiu = (function() {
 function create_el(parent, tagName, attrs, text) {
 	var el = document.createElement(tagName);
@@ -27,6 +29,8 @@ return {
 };
 })();
 
+var aentry = (function() {
+
 function discipline_by_id(did) {
 	var disciplines = tournament_info.disciplines;
 	for (var i = 0;i < disciplines.length;i++) {
@@ -50,7 +54,7 @@ function on_discipline_change(entry_idx, new_id, selection_container) {
 			uiu.visible(current_rows[i], true);
 		} else {
 			// Create a new row
-			add_player_selection_row(selection_container, entry_idx, i);
+			add_player_selection_row(discipline, new_specs[i], selection_container, entry_idx, i);
 		}
 	}
 	for (var j = new_count;j < current_rows.length;j++) {
@@ -58,10 +62,78 @@ function on_discipline_change(entry_idx, new_id, selection_container) {
 	}
 }
 
-function add_player_selection_row(container, entry_idx, row_idx) {
+
+function autocomplete_renderItem(ul, item) {
+	var li = $("<li>");
+	li.addClass('autocomplete_player');
+	li.attr("data-player_textid", item.player_textid);
+	li.attr("data-player_name", item.player_name);
+	$('<span class="autocomplete_player_textid">')
+		.text(item.textid)
+		.appendTo(li);
+	$('<span class="autocomplete_player_name">')
+		.text(item.name)
+		.appendTo(li);
+	$('<span class="autocomplete_club_name">')
+		.text(item.club_name)
+		.appendTo(li);
+	li.appendTo(ul);
+	return li;
+}
+
+function autocomplete_set_value(ps, item) {
+	$(ps).val('(' + item.textid + ') ' + item.name);
+	var club_field = $(ps).parent().find('.club');
+	club_field.val('(' + item.club_textid + ') ' + item.club_name);
+	club_field.addClass('club_selector_autoset');
+}
+
+function add_player_selection_row(discipline, spec, container, entry_idx, row_idx) {
 	var sel_row = uiu.create_el(container, 'div', {
 		'class': 'aentry_selection_row',
-	}, 'TODO: Select player ' + row_idx);
+	});
+
+	var ps = uiu.create_el(container, 'input', {
+		'name': 'player_name_' + entry_idx,
+	});
+
+	function player_search(request, response) {
+		var root_path = $('body').attr('data-root-path');
+		var ac_url = root_path + 'd/' + discipline.id + '/player_search';
+		var ac_params = {
+			gender: spec.gender,
+			term: request.term,
+		};
+		$.getJSON(ac_url, ac_params, function(result) {
+			response(result.players);
+		}).error(response());
+	}
+
+	$(ps).autocomplete({
+		minLength: 3,
+		source: player_search,
+		select: function(event, ui) {
+			autocomplete_set_value(ps, ui.item);
+			event.preventDefault();
+
+			var tabables = $('input:not([tabindex="-1"])');
+			var current = $(':focus');
+			var nextIndex = 0;
+			if (current.length === 1) {
+				var currentIndex = tabables.index(current);
+				if (currentIndex + 1 < tabables.length){
+					nextIndex = currentIndex + 1;
+				}
+			}
+			tabables.eq(nextIndex).focus();
+		},
+		focus: function(event, ui) {
+			autocomplete_set_value(ps, ui.item);
+			event.preventDefault(); 
+		},
+
+		appendTo: $(ps).parent()
+	}).data("ui-autocomplete")._renderItem = autocomplete_renderItem;
 
 }
 
@@ -97,3 +169,5 @@ document.addEventListener('DOMContentLoaded', function() {
 
 	add_entry_row();
 });
+
+})();
